4850b071c349e82d419ba0ce0f3e320ae47fedd4
Joshua Purcell <310946+vuldin@users.noreply.github.com>
2021-07-14 10:03:03 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,37 +1,11 @@
-const express = require("express");
+const http = require('http')
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
-const app = express();
+const requestListener = function (_, res) {
+  res.writeHead(200)
+  res.end('hello world')
+}
 
-// app.get("/api", (req, res) => {
-//   res.json({ message: "Hello from server!" });
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server listening on ${PORT}`);
-// });
-
-console.log("Hello nodejs");
-
-var ibmdb = require('ibm_db');
-var connStr = "DATABASE=BLUDB;HOSTNAME=10.130.0.11;UID=admin;PWD=cyM1bFZAaGQyM0BENFNHPw==;PORT=32195;PROTOCOL=TCPIP";
-
-ibmdb.open(connStr, function (err,conn) {
-  if (err) return console.log(err);
-  
-  conn.query('select * from customer', function (err, data) {
-    if (err) console.log(err);
-    else {
-      console.log(data);
-    }
-
-    conn.close(function () {
-      console.log('done');
-    });
-  });
-});
-
-
-
-// DRIVER=/Users/Ritu.Patel@ibm.com/Desktop/full-stack/odbc_cli_32;DATABASE=testdb;UID=db2inst1;PWD=password;HOSTNAME=localhost;port=50000
+const server = http.createServer(requestListener)
+server.listen(PORT)
